EcoEvoGenomics/genotyping_pipeline

commit e87922e5611ec4a20a3f8030279fb999863accb2

Fix error in coordination between runner script and Nextflow call vcf script

pipeline/nextflow/call_variants.nf
@@ -17,7 +17,7 @@ params.publish_dir = './output'
 // read in a file of genome windows
 // params.windows = file('sparrow_genome_windows.list')
 
-windows_list = file(params.windows)
+windows_list = file("${params.windows_dir}/genome_windows.list")
     .readLines()
     //.each { println it }
 
@@ -60,9 +60,7 @@ process genotyping {
     if [[ "${windows}" == "scaff"* ]];
     then
         # if window is a scaffold
-        bcftools mpileup -d 8000 --ignore-RG -R ${baseDir}/${windows} -a AD,DP,SP -Ou -f ${params.ref} -b ${bams} \
-        | bcftools call --threads ${task.cpus} --ploidy-file ${ploidyFile} -f GQ,GP -mO z -o ${windows}.vcf.gz
-    else
+        bcftools mpileup -d 8000 --ignore-RG -R ${baseDir}/../../${params.windows_dir}/${windows} -a AD,DP,SP -Ou -f ${params.ref} -b ${bams} \    else
         # for normal genome windows
         bcftools mpileup -d 8000 --ignore-RG -r ${windows} -a AD,DP,SP -Ou -f ${params.ref} -b ${bams} \
         | bcftools call --threads ${task.cpus} --ploidy-file ${ploidyFile} -f GQ,GP -mO z -o ${windows}.vcf.gz

genotyping_pipeline.slurm.sh
@@ -51,6 +51,7 @@
     vcf_filt_keep=""
     
     # NB: Only change if you know what you are doing (ask Erik), typically for re-filtering
+    #     Also, . must be the genotyping_pipeline repository
     output_dir=./output
     trim_align_output_dir=${output_dir}/trim_align
     call_vcf_output_dir=${output_dir}/call_vcf
@@ -103,7 +104,7 @@ if [ $call_vcf = 'yes' ]; then
         -c ./pipeline/config/call_variants.config \
         --ref $ref_genome \
         --bams $bam_list \
-        --windows $windows_dir/genome_windows.list \
+        --windows_dir $windows_dir \
         --ploidyFile $ref_ploidy_file \
         --publish_dir $call_vcf_output_dir
 fi

commit ac68f7b2d25e5c7856048f6a5e7aa8970eed658d

Fix error introduced in e87922e

pipeline/nextflow/call_variants.nf
@@ -60,7 +60,9 @@ process genotyping {
     if [[ "${windows}" == "scaff"* ]];
     then
         # if window is a scaffold
-        bcftools mpileup -d 8000 --ignore-RG -R ${baseDir}/../../${params.windows_dir}/${windows} -a AD,DP,SP -Ou -f ${params.ref} -b ${bams} \    else
+        bcftools mpileup -d 8000 --ignore-RG -R ${baseDir}/../../${params.windows_dir}/${windows} -a AD,DP,SP -Ou -f ${params.ref} -b ${bams} \
+        | bcftools call --threads ${task.cpus} --ploidy-file ${ploidyFile} -f GQ,GP -mO z -o ${windows}.vcf.gz
+    else
         # for normal genome windows
         bcftools mpileup -d 8000 --ignore-RG -r ${windows} -a AD,DP,SP -Ou -f ${params.ref} -b ${bams} \
         | bcftools call --threads ${task.cpus} --ploidy-file ${ploidyFile} -f GQ,GP -mO z -o ${windows}.vcf.gz
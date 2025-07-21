055bec69d710157d13fe97277baa6f83a60dd985
Erik Sandertun Røed <e.s.roed@ibv.uio.no>
2025-07-22 01:36:56 +0200

    Update genotyping_pipeline.slurm.sh

--- genotyping_pipeline.slurm.sh
+++ genotyping_pipeline.slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=SLURM-%j-%x.out
 #SBATCH --error=SLURM-%j-%x.err
 #SBATCH --account=nn10082k
-#
+
 # RESOURCE ALLOCATION
 #SBATCH --nodes=1
 #SBATCH --tasks=1
@@ -14,39 +14,40 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=99:00:00
 
-### SETTINGS (1 / 2)
-###
-### ----------------- User input ----------------- ###
+### SETTINGS (1 / 2): User input ----------------- ###
 
-    # Path to .CSV of input samples
-    # Note: samples are rows and the columns "ID, LANE, F_READ_PATH, R_READ_PATH" without headers
+    # PROVIDE PATH TO INPUT CSV
+    # Note: One row per sample, with unheadered columns "ID, LANE, R1_FASTQ_PATH, R2_FASTQ_PATH"
     sample_csv=
     
-    # Path to this repository
+    # PROVIDE PATH TO THIS REPOSITORY
     repository_path=
 
-    # Which steps to run?
-    # Note: steps must be run in order, but you can repeat filter_variants and multiqc with different settings
+    # NAME A NEXTFLOW CONFIGURATION PROFILE
+    nextflow_profile="saga"
+
+    # SELECT STEPS TO RUN
+    # Note: Must be run in order, but filter_variants through multiqc can be repeated with different settings
     trim_align_reads=yes
     call_variants=yes
     filter_variants=yes
     multiqc=yes
 
-    # Settings for step trim_align_reads
-    # Note: read_target only applies if downsample_reads=yes
-    # Note: flag 0x400 is for optical and PCR duplicates
+    # SET OPTIONS FOR TRIM AND ALIGN STEP
+    # Note: The read_target only applies if downsample_reads=yes
+    # Note: The default flag, 0x400, is for optical and PCR duplicates
     deduplicate_reads=no
     downsample_reads=no
     read_target=1000000
     exclude_flags=0x400
 
-    # Settings for step call_variants
+    # SET OPTIONS FOR CALL VARIANTS STEP
     window_size=10000000
     concatenate_unfiltered_vcfs=no
 
-    # Settings for step filter_variants
-    # Note: change filtering_label and re-run filter_variants to refilter output from call_variants
-    filtering_label='default_filters'
+    # SET OPTIONS FOR FILTER VARIANTS STEP
+    # Note: To refilter output from call_variants, change filtering_label and re-run the filter_variants step
+    filtering_label="default_filters"
     filtering_min_alleles=2
     filtering_max_alleles=2
     filtering_max_missing=0.8
@@ -55,41 +56,34 @@
     filtering_minDP=5
     filtering_maxDP=30
     filtering_minQ=30
-    filtering_keep=''
+    filtering_keep=""
 
-    # Reference genome
+    # PROVIDE DETAILS OF REFERENCE GENOME
     ref_genome=/cluster/projects/nn10082k/ref/house_sparrow_genome_assembly-18-11-14_masked.fa
     ref_index=/cluster/projects/nn10082k/ref/house_sparrow_genome_assembly-18-11-14_masked.fa.fai
-    ref_scaffold_name='scaffold'
+    ref_scaffold_name="scaffold"
     ref_ploidy_file=./pipeline/assets/default.ploidy
-    
-    # Path to a conda environment with Nextflow
-    nextflow_envir_path=/cluster/projects/nn10082k/conda_group/nextflow
-
-    # Name of Nextflow configuration profile to use (see nextflow.config)
-    nextflow_profile="saga"
 
 ### --------------- End user input --------------- ###
 
-### SETTINGS (2 / 2)
-###
-### ------------ Set up environment -------------- ###
+### SETTINGS (2 / 2): Set up environment --------- ###
 
-    # This script must be run from an environment with:
-    # - Conda
-    # - Singularity (default on Saga)
-    # - Slurm (default on Saga)
+    # The pipeline depends on:
+    #  - Nextflow
+    #  - Apptainer (can be changed in nextflow.config)
+    #  - Slurm (can be changed in nextflow.config)
 
+    # For EcoEvoGenomics members on the Saga HPC:
     module --quiet purge
     module load Miniconda3/22.11.1-1
     source ${EBROOTMINICONDA3}/bin/activate
+    conda activate /cluster/projects/nn10082k/conda_group/nextflow
 
 ### ------------ End set up environment ---------- ###
 
-# Prepare environment
+# Main script begins
 set -o errexit
 set -o nounset
-conda activate ${nextflow_envir_path}
 
 # Function to handle missing output directories
 mkmissingdir() {
@@ -106,18 +100,15 @@ chkprevious() {
     fi
 }
 
-# Begin work
 cd $repository_path
-
 output_dir=${repository_path}/output
 trim_align_output_dir=${output_dir}/01-aligned_reads
 call_variants_output_dir=${output_dir}/02-variants_unfiltered
 filter_variants_output_dir=${output_dir}/03-variants_filtered/${filtering_label}
 multiqc_output_dir=${output_dir}/04-multiqc
-
 mkmissingdir $output_dir
 
-if [ $trim_align_reads = 'yes' ]; then
+if [ $trim_align_reads = "yes" ]; then
     mkmissingdir $trim_align_output_dir
     nextflow -log ./.nextflow/nextflow.log \
         run ./pipeline/nextflow/trim_align_reads.nf \
@@ -134,7 +125,7 @@ if [ $trim_align_reads = 'yes' ]; then
         --publish_dir $trim_align_output_dir
 fi
 
-if [ $call_variants = 'yes' ]; then
+if [ $call_variants = "yes" ]; then
     chkprevious "Step: call_variants" $trim_align_output_dir
     mkmissingdir $call_variants_output_dir
     nextflow -log ./.nextflow/nextflow.log \
@@ -151,7 +142,7 @@ if [ $call_variants = 'yes' ]; then
         --publish_dir $call_variants_output_dir
 fi
 
-if [ $filter_variants = 'yes' ]; then
+if [ $filter_variants = "yes" ]; then
     chkprevious "Step: filter_variants" $call_variants_output_dir
     mkmissingdir $filter_variants_output_dir
     nextflow -log ./.nextflow/nextflow.log \
